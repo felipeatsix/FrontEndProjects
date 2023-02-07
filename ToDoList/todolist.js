8727d0e977c2cf9297ba3afe0e16b98cd0adbcec
felipe-santos@outlook.com <felipe-santos@outlook.com>
2023-02-07 17:18:15 +0100

    use innerHtml property instead of creating text node

--- ToDoList/todolist.js
+++ ToDoList/todolist.js
@@ -1,12 +1,11 @@
 function newItem() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
     if (inputValue === '') {
         alert("You must write something!");
     }
     else {
+        li.innerHTML = inputValue
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
