e67eb35193b89e239471973cb3676872ceb4280f
felipe-santos@outlook.com <felipe-santos@outlook.com>
2023-02-08 21:19:18 +0100

    New app

---
+++ GithubUserPicker/javascript/githubuserpicker.js
@@ -0,0 +1,24 @@
+function getGithubUser() {
+    let username = document.getElementById("username")
+    let name = document.getElementById("name")
+    let img = document.querySelector("img")
+    let input = document.getElementById("input").value
+
+    if (input === "") {
+        alert("You must provide a valid username")
+    }
+    else {
+        fetch("https://api.github.com/users/" + input)
+            .then((result) => result.json())
+            .then((result) => {
+                if (!result.login) {
+                    alert("User " + input + " " + result.message)
+                }
+                else {
+                    name.innerHTML = "Name: " + result.name
+                    username.innerHTML = "Username: " + result.login
+                    img.src = result.avatar_url
+                }
+            })
+    }
+}
