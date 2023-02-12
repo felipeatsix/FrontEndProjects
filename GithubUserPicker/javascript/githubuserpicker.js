9783895b5c2b6f3033f32941d70caff564c3b8a1
felipe-santos@outlook.com <felipe-santos@outlook.com>
2023-02-12 19:12:20 +0100

    Add process status and error handling

--- GithubUserPicker/javascript/githubuserpicker.js
+++ GithubUserPicker/javascript/githubuserpicker.js
@@ -3,6 +3,7 @@ function getGithubUser() {
     let name = document.getElementById("name")
     let img = document.querySelector("img")
     let input = document.getElementById("input").value
+    let status = document.getElementById("status");
 
     function clearData() {
         img.src = "https://th.bing.com/th/id/OIP.1DLYAqE5UY19idJJOkFQegHaHa?pid=ImgDet&rs=1"
@@ -10,22 +11,33 @@ function getGithubUser() {
         name.innerHTML = "Name"
     }
     if (input === "") {
-        alert("You must provide a valid username")
+        alert("You must provide a valid username!")
         clearData();
+        return
     }
     else {
-        fetch("https://api.github.com/users/" + input)
-            .then((result) => result.json())
+        status.innerHTML = `Searching for user "${input}" on Github...`
+        fetch(`https://api.github.com/users/${input}`)
             .then((result) => {
-                if (!result.login) {
-                    clearData();
-                    alert("User " + input + " " + result.message.toLowerCase())
+                console.log(result)
+                if (!result.ok) {
+                    throw new Error(`HTTP error! status: ${result.status}`)
                 }
-                else {
-                    name.innerHTML = result.name
-                    username.innerHTML = "@" + result.login
-                    img.src = result.avatar_url
-                }
+                return result.json()
+            })
+            .then((result) => {
+                console.log(result)
+                name.innerHTML = result.name
+                username.innerHTML = "@" + result.login
+                img.src = result.avatar_url
+            })
+            .then(() => {
+                status.innerHTML = ""
+            })
+            .catch((error) => {
+                let errorMessage = "There was a problem with the fetch operation"
+                status.innerHTML = `${errorMessage}: ${error}`
+                console.error(`${errorMessage}:`, error);
             })
         // Reset input
         document.getElementById("input").value = ""
