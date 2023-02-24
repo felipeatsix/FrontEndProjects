f019660701a7404005dd9688c4d36631fe083a94
felipe-santos@outlook.com <felipe-santos@outlook.com>
2023-02-24 17:12:48 +0100

    reorg folders + add github user picker in react

---
+++ GithubUserPicker/react/src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function GithubUserPicker() {
+    const [inputValue, setInputValue] = useState('');
+    const [name, setName] = useState('Name');
+    const [username, setUsername] = useState('Username');
+    const [imgSrc, setImgSrc] = useState('https://th.bing.com/th/id/OIP.1DLYAqE5UY19idJJOkFQegHaHa?pid=ImgDet&rs=1');
+    const [status, setStatus] = useState('');
+
+    function clearData() {
+        setImgSrc('https://th.bing.com/th/id/OIP.1DLYAqE5UY19idJJOkFQegHaHa?pid=ImgDet&rs=1');
+        setUsername('Username');
+        setName('Name');
+    }
+
+    function handleInputChange(event) {
+        setInputValue(event.target.value);
+    }
+
+    function handleSearchClick() {
+        if (inputValue === '') {
+            alert('You must provide a valid username!');
+            clearData();
+            return;
+        }
+        setStatus(`Searching for user "${inputValue}" on Github...`);
+        fetch(`https://api.github.com/users/${inputValue}`)
+            .then((result) => {
+                if (!result.ok) {
+                    throw new Error(`HTTP error! status: ${result.status}`);
+                }
+                return result.json();
+            })
+            .then((result) => {
+                setName(result.name);
+                setUsername(`@${result.login}`);
+                setImgSrc(result.avatar_url);
+            })
+            .then(() => {
+                setStatus('');
+            })
+            .catch((error) => {
+                const errorMessage = 'There was a problem with the fetch operation';
+                setStatus(`${errorMessage}: ${error}`);
+                console.error(`${errorMessage}:`, error);
+            });
+        setInputValue('');
+    }
+
+    return (
+        <div>
+            <div id="search">
+                <label>Github username</label>
+                <input type="text" placeholder="Search github username" value={inputValue} onChange={handleInputChange} />
+                <button onClick={handleSearchClick}>GO</button>
+            </div>
+            <div id="card">
+                <div id="details">
+                    <img src={imgSrc} alt="User avatar" />
+                    <label id="name">{name}</label>
+                    <label id="username">{username}</label>
+                </div>
+            </div>
+            <div id="status">{status}</div>
+        </div>
+    );
+}
+
+export default GithubUserPicker;
